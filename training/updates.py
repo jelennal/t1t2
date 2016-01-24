c6f418461370e5c11781ce914b846f4956a9be83
jelennal <jelena.luketina@aalto.fi>
2016-01-24 23:38:27 +0200

    tidier Lop

--- training/updates.py
+++ training/updates.py
@@ -173,10 +173,8 @@ def updates(mlp, params, globalLR1, globalLR2, momentParam1, momentParam2, phase
         if not jacCost:
             assert len(mlp.paramsT2) == len(gradT2exact)    
         
-        # UPDATING T2 ------------------------------- EXACT
-        # TO TRY: T.Lop()
+        # UPDATING T2 ------------------------------- DIY
         
-        # --------------------------------------------------------- ------  DIY noiseloop
 #        rop_grad_noise = []; rop_grad_s = []; rop_s = []
 #        
 #        for (i, grad) in zip(range(params.nLayers - 1), gradC2):
@@ -184,23 +182,26 @@ def updates(mlp, params, globalLR1, globalLR2, momentParam1, momentParam2, phase
         
         
         if not jacCost:
-            for (t2, t2e) in zip(mlp.paramsT2, gradT2exact):
-                    accumulate = None
-                    for (gt1, gt2) in zip(gradT1, gradC2):
-                        try:
-                            val = T.Lop(-gt1, t2, gt2)
-                        except theano.gradient.DisconnectedInputError:
-                            val = None
-                        accumulate = val if accumulate is None else (
-                            accumulate if val is None else accumulate + val)
-                    if accumulate is None:
-                        raise theano.gradient.DisconnectedInputError, 'Cost does not' \
-                            + ' depend on %s' % t2.name
-                    gradreg += [accumulate + t2e]
+            
+            
+#            for (t2, t2e) in zip(mlp.paramsT2, gradT2exact):
+#                    accumulate = None
+#                    for (gt1, gt2) in zip(gradT1, gradC2):
+#                        try:
+#                            val = T.Lop(-gt1, t2, gt2)
+#                        except theano.gradient.DisconnectedInputError:
+#                            val = None
+#                        accumulate = val if accumulate is None else (
+#                            accumulate if val is None else accumulate + val)
+#                    if accumulate is None:
+#                        raise theano.gradient.DisconnectedInputError, 'Cost does not' \
+#                            + ' depend on %s' % t2.name
+#                    gradreg += [accumulate + t2e]
 
-#            assert len(gradT1) == len(gradC2)
-#            gradreg = T.Lop(gradT1, mlp.paramsT2, gradC2) # add - to each
-#            print len(gradreg)           
+            assert len(gradT1) == len(gradC2)
+            print len(gradT1), len(mlp.paramsT2), len(gradC2)
+            gradreg = T.Lop(gradT1, mlp.paramsT2, gradC2) # add - to each
+            print len(gradreg)           
             
         else:
                                 
