d6ab915f03a6dfe83d2725a329420c4cf0ef10f2
jelennal <jelena.luketina@aalto.fi>
2016-05-11 01:31:44 +0300

    added elu activation, ops activations

--- models/layers/activations.py
+++ models/layers/activations.py
@@ -5,28 +5,28 @@ eps = 1e-8
 zero = np.float32(0.)
 one = np.float32(1.)
 convNonLin = 'relu'
-
-
-def softmax(x):
-    e_x = T.exp(x - x.max(axis=1, keepdims=True))
-    return e_x / e_x.sum(axis=1, keepdims=True)
-
     
 def activation(input, key):
-    relu = lambda x: T.maximum(0, x)    
+    ''' 
+        Defining various activation functions.    
+    '''
     identity = lambda x: x    
-    activFun = {'lin': identity, 'tanh': T.tanh, 'relu': relu, 
-                'sig': T.nnet.sigmoid, 'softmax': softmax}[key]
+    activFun = {'lin': identity, 'tanh': T.tanh, 
+                'relu': T.nnet.relu, 'sig': T.nnet.sigmoid, 
+                'elu': T.nnet.elu, 'softmax': T.nnet.softmax}[key]
+    
     return activFun(input)   
 
 
 def weight_multiplier(nIn, nOut, key):    
-
-    ''' Initial range of values for weights, given diffrent activation functions    
-
+    ''' 
+        Initial range of values for weights, given diffrent activation functions.    
     '''    
-
     weightMultiplier = {'lin': np.sqrt(1./nIn), 'tanh': np.sqrt(1./(nIn+nOut))*np.sqrt(6.),
                         'relu': np.sqrt(1./nIn), 'sig': np.sqrt(1./(nIn+nOut))*np.sqrt(6.)/4, 
                         'elu': np.sqrt(1./nIn), 'softmax': 1e-5}[key]
+
     return weightMultiplier       
+    
+    
+    
