1a685fd73ced04344f82953d6d4d8c04359a9daf
jelennal <jelena.luketina@aalto.fi>
2016-05-17 17:37:07 +0300

    changed weight initializations

--- models/layers/activations.py
+++ models/layers/activations.py
@@ -1,7 +1,7 @@
 import numpy as np
 import theano.tensor as T
 
-eps = 1e-8
+eps = np.float32(1e-8)
 zero = np.float32(0.)
 one = np.float32(1.)
 convNonLin = 'relu'
@@ -11,9 +11,12 @@ def activation(input, key):
         Defining various activation functions.    
     '''
     identity = lambda x: x    
-    activFun = {'lin': identity, 'tanh': T.tanh, 
-                'relu': T.nnet.relu, 'sig': T.nnet.sigmoid, 
-                'elu': T.nnet.elu, 'softmax': T.nnet.softmax}[key]
+    activFun = {'lin':  identity,
+                'relu': T.nnet.relu,
+                'elu':  T.nnet.elu,
+                'tanh': T.tanh, 
+                'sig':  T.nnet.sigmoid, 
+                'softmax': T.nnet.softmax}[key]
     
     return activFun(input)   
 
@@ -22,9 +25,12 @@ def weight_multiplier(nIn, nOut, key):
     ''' 
         Initial range of values for weights, given diffrent activation functions.    
     '''    
-    weightMultiplier = {'lin': np.sqrt(1./nIn), 'tanh': np.sqrt(1./(nIn+nOut))*np.sqrt(6.),
-                        'relu': np.sqrt(1./nIn), 'sig': np.sqrt(1./(nIn+nOut))*np.sqrt(6.)/4, 
-                        'elu': np.sqrt(1./nIn), 'softmax': 1e-5}[key]
+    weightMultiplier = {'lin':  np.sqrt(1./(nIn+nOut)), 
+                        'relu': np.sqrt(1./(nIn+nOut))*np.sqrt(12), 
+                        'elu':  np.sqrt(1./(nIn+nOut))*np.sqrt(12), 
+                        'tanh': np.sqrt(1./(nIn+nOut))*np.sqrt(6.),
+                        'sig':  np.sqrt(1./(nIn+nOut))*np.sqrt(6.)/4, 
+                        'softmax': 1e-5}[key]
 
     return weightMultiplier       
     
