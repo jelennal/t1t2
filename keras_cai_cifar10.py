3245fb6454ae79ac469a086fb4ba6faad06b01e5
Qwlouse <qwlouse@gmail.com>
2016-06-16 20:41:19 +0200

    added support for tensorboard, validation subset, ELU and named the layers

--- keras_cai_cifar10.py
+++ keras_cai_cifar10.py
@@ -5,7 +5,7 @@ from keras.models import Sequential
 from keras.layers import Activation, Flatten, Dropout
 from keras.layers import Convolution2D, AveragePooling2D, MaxPooling2D
 from keras.layers.normalization import BatchNormalization
-from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.advanced_activations import LeakyReLU, ELU
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers.noise import GaussianNoise
 from keras.regularizers import l2
@@ -24,6 +24,7 @@ ex = Experiment('keras_custom_cifar10')
 def cfg():
     # Data Preprocessing
     subset = 50000
+    val_subset = 10000
     preprocessed = 'pylearn2'
     augment = False
     norm_std = True
@@ -47,6 +48,7 @@ def cfg():
     learning_rate = 0.001
     use_adam = True
     verbose = False
+    tensorboard = False
 
 
 @ex.named_config
@@ -94,7 +96,7 @@ def get_schedule(learning_rate, schedule, sched_type):
 
 
 @ex.capture
-def prepare_dataset(preprocessed, subset, norm_std):
+def prepare_dataset(preprocessed, subset, val_subset, norm_std):
     if preprocessed is True:
         print('Loading preprocessed dataset')
         ds = np.load('preprocessed_cifar.npz')
@@ -140,6 +142,13 @@ def prepare_dataset(preprocessed, subset, norm_std):
     X_train = X_train[:subset]
     Y_train = Y_train[:subset]
 
+    idx = np.arange(X_test.shape[0], dtype=np.int)
+    np.random.shuffle(idx)
+    idx = idx[:val_subset]
+    X_test = X_test[idx]
+    Y_test = Y_test[idx]
+    print('average val-label:', Y_test.argmax(1).mean())
+
     print('X_train shape:', X_train.shape)
     print('Y_train shape:', Y_train.shape)
     print('X_test shape:', X_test.shape)
@@ -154,11 +163,13 @@ def prepare_dataset(preprocessed, subset, norm_std):
 def build_model_spec(spec, act_func, decay, init, std, eps, leak, base_size):
     model = Sequential()
 
-    def add_act():
+    def add_act(name):
         if act_func == 'leakyrelu':
-            model.add(LeakyReLU(alpha=leak))
+            model.add(LeakyReLU(alpha=leak, name=name))
+        elif act_func == 'elu':
+            model.add(ELU(name=name))
         else:
-            model.add(Activation(act_func))
+            model.add(Activation(act_func, name=name))
 
     size = 3
     for i, block in enumerate(spec.split()):
@@ -166,21 +177,23 @@ def build_model_spec(spec, act_func, decay, init, std, eps, leak, base_size):
         if block[0] == 'C':
             size = base_size*int(block[1])
             print('===> Conv 3x3 {}'.format(size))
-            model.add(Convolution2D(size, 3, 3, border_mode='same',
+            model.add(Convolution2D(size, 3, 3, border_mode='same', name="conv_{}".format(i),
                                     init=init, W_regularizer=l2(decay), **kwargs))
             rest = block[2:]
         elif block[0] == 'c':
             size = base_size * int(block[1])
             print('===> Conv 1x1 {}'.format(size))
-            model.add(Convolution2D(size, 1, 1, init=init, W_regularizer=l2(decay), **kwargs))
+            model.add(Convolution2D(size, 1, 1, init=init, W_regularizer=l2(decay), name="conv_{}".format(i), **kwargs))
             rest = block[2:]
         elif block[0] == 'M':
             print('===> MaxPool 2x2')
-            model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+            model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name="maxpool_{}".format(i)))
             rest = block[1:]
         elif block[0] == 'm':
             print('===> Strided Convolution 3x3 with stride 2x2')
-            model.add(Convolution2D(size, 3, 3, init=init, W_regularizer=l2(decay), subsample=(2, 2), border_mode='valid'))
+            model.add(Convolution2D(size, 3, 3, init=init, name="convpool_{}".format(i),
+                                    W_regularizer=l2(decay), subsample=(2, 2),
+                                    border_mode='valid'))
             rest = block[1:]
         else:
             raise KeyError('Unknown type "{}"'.format(block[0]))
@@ -188,28 +201,28 @@ def build_model_spec(spec, act_func, decay, init, std, eps, leak, base_size):
         for r in rest:
             if r == 'B':
                 print('BatchNorm')
-                model.add(BatchNormalization(mode=1, epsilon=eps))
+                model.add(BatchNormalization(mode=1, epsilon=eps, name="batchnorm_{}".format(i)))
             elif r == 'N':
                 print('GaussianNoise')
-                model.add(GaussianNoise(sigma=std))
+                model.add(GaussianNoise(sigma=std, name="noise_{}".format(i)))
             elif r == 'A':
                 print('Activation: {}'.format(act_func))
-                add_act()
+                add_act(name="act_{}".format(i))
             elif r == 'D':
                 print('Dropout (p=0.5)')
-                model.add(Dropout(0.5))
+                model.add(Dropout(0.5, name="dropout_{}".format(i)))
             elif r == 'd':
-                model.add(Dropout(0.2))
+                model.add(Dropout(0.2, name="dropout02_{}".format(i)))
 
         print(model.outputs[0]._keras_shape)
 
     model.add(Convolution2D(10, 1, 1, border_mode='valid', init=init,
                             W_regularizer=l2(decay)))
-    add_act()
+    add_act(name="act_{}".format(i+1))
 
     model.add(AveragePooling2D(pool_size=model.outputs[0]._keras_shape[2:]))
     model.add(Flatten())
-    model.add(Activation('softmax'))
+    model.add(Activation('softmax', name="softmax"))
     return model
 
 
@@ -238,12 +251,97 @@ class InfoUpdater(Callback):
             self.run.info['logs'][k] = logout
 
 
+
+
+
+
 @ex.automain
-def run(batch_size, max_epochs, verbose, augment, _run):
+def run(batch_size, max_epochs, verbose, augment, tensorboard, _run):
     X_train, Y_train, X_test, Y_test = prepare_dataset()
     model = build_model_spec()
     compile_model(model)
 
+    callbacks = [get_schedule(), BaseLogger(), InfoUpdater(_run)]
+
+    if tensorboard:
+        print('Adding TensorBoard!')
+
+        from keras import backend as K
+        class TensorBoard(Callback):
+            ''' copied from keras.callbacks'''
+            def __init__(self, log_dir='./logs', histogram_freq=0,
+                         write_graph=True):
+                super(Callback, self).__init__()
+                if K._BACKEND != 'tensorflow':
+                    raise Exception('TensorBoard callback only works '
+                                    'with the TensorFlow backend.')
+                self.log_dir = log_dir
+                self.histogram_freq = histogram_freq
+                self.merged = None
+                self.write_graph = write_graph
+
+            def _set_model(self, model):
+                import tensorflow as tf
+                import keras.backend.tensorflow_backend as KTF
+
+                self.model = model
+                self.sess = KTF.get_session()
+                if self.histogram_freq and self.merged is None:
+                    layers = self.model.layers
+                    for layer in layers:
+                        if hasattr(layer, 'W'):
+                            tf.histogram_summary('{}_W'.format(layer.name),
+                                                 layer.W)
+                        if hasattr(layer, 'b'):
+                            tf.histogram_summary('{}_b'.format(layer.name),
+                                                 layer.b)
+                        if hasattr(layer, 'output'):
+                            tf.histogram_summary('{}_out'.format(layer.name),
+                                                 layer.output)
+                self.merged = tf.merge_all_summaries()
+                if self.write_graph:
+                    if tf.__version__ >= '0.8.0':
+                        self.writer = tf.train.SummaryWriter(self.log_dir,
+                                                             self.sess.graph)
+                    else:
+                        self.writer = tf.train.SummaryWriter(self.log_dir,
+                                                             self.sess.graph_def)
+                else:
+                    self.writer = tf.train.SummaryWriter(self.log_dir)
+
+            def on_epoch_end(self, epoch, logs={}):
+                import tensorflow as tf
+
+                if self.model.validation_data and self.histogram_freq:
+                    if epoch % self.histogram_freq == 0:
+                        # TODO: implement batched calls to sess.run
+                        # (current call will likely go OOM on GPU)
+                        if self.model.uses_learning_phase:
+                            cut_v_data = len(self.model.inputs)
+                            val_data = self.model.validation_data[
+                                       :cut_v_data] + [0]
+                            tensors = self.model.inputs + [K.learning_phase()]
+                        else:
+                            val_data = self.model.validation_data
+                            tensors = self.model.inputs
+                        feed_dict = dict(zip(tensors, val_data))
+                        result = self.sess.run([self.merged],
+                                               feed_dict=feed_dict)
+                        summary_str = result[0]
+                        self.writer.add_summary(summary_str, epoch)
+
+                for name, value in logs.items():
+                    if name in ['batch', 'size']:
+                        continue
+                    summary = tf.Summary()
+                    summary_value = summary.value.add()
+                    summary_value.simple_value = value
+                    summary_value.tag = name
+                    self.writer.add_summary(summary, epoch)
+                self.writer.flush()
+
+        callbacks.append(TensorBoard(log_dir='./logs', histogram_freq=1,
+                                     write_graph=True))
     if not augment:
         print('Not using data augmentation.')
         model.fit(X_train, Y_train,
@@ -251,7 +349,7 @@ def run(batch_size, max_epochs, verbose, augment, _run):
                   batch_size=batch_size,
                   nb_epoch=max_epochs,
                   validation_data=(X_test, Y_test),
-                  callbacks=[get_schedule(), BaseLogger(), InfoUpdater(_run)],
+                  callbacks=callbacks,
                   shuffle=True)
     else:
         print('Using real-time data augmentation.')
@@ -272,7 +370,7 @@ def run(batch_size, max_epochs, verbose, augment, _run):
             verbose=verbose,
             nb_epoch=max_epochs,
             validation_data=datagen.flow(X_test, Y_test, batch_size=batch_size),
-            callbacks=[get_schedule(), BaseLogger(), InfoUpdater(_run)])
+            callbacks=callbacks)
 
     import tempfile
     with tempfile.NamedTemporaryFile(prefix='model_', suffix='.json') as f:
